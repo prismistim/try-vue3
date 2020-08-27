05f93a79387625601e239e77d6e2fc7d007ff19c
snowsphere <prismistim@hotmail.com>
2020-08-28 00:49:20 +0900

    add: フォームバインディングのテストコンポーネントの追加

---
+++ src/components/SubTitleInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <input
+    @input="emitTitle"
+    class="sub-title"
+    type="text"
+  >
+</template>
+
+<script lang="ts">
+import { defineComponent, SetupContext } from 'vue'
+
+export default defineComponent({
+  setup (context: SetupContext) {
+    const emitTitle = (evt) => {
+      console.log(evt.target.value)
+      context.emit('change', evt.target.value)
+    }
+    return {
+      emitTitle
+    }
+  }
+})
+</script>
+
+<style>
+.sub-title {
+
+}
+</style>
